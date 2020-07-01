f8aec865274b91f29b6161d9a2f38c96117747de
aamir <aamir.iqbal040@gmail.com>
2020-07-01 19:31:41 +0530

    Changes commited

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Button button;
+    public string newScene;
+    private object onButtonClick;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    private void OnButtonClick()
+    {
+        Debug.Log("Button Clicked");
+        SceneManager.LoadScene(newScene);
+    }
+}
